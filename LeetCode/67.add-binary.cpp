b6464679112990f82b27f4ca34ee4ccdfe929d2f
bowchen <bowchen@cisco.com>
2019-08-06 15:57:19 +0800

    Update 67 69 70 72

---
+++ LeetCode/67.add-binary.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=67 lang=cpp
+ *
+ * [67] Add Binary
+ */
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        if (a.size() < b.size())
+            swap(a, b);
+        int i = a.size(), j = b.size();
+        while (i--) {
+            if (j) a[i] += b[--j] & 1;
+            if (a[i] > '1') {
+                a[i] -= 2;
+                if (i) a[i-1]++; else a = '1' + a;
+            }
+        }
+        return a;
+    }
+};
+
